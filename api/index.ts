ae0bb5dc25caeace26949ab6ed0d21eb8ba10e13
Денис  Зяблинцев <deniszablincev@DenisMac.local>
2025-11-06 14:01:14 +0300

    Исправлен импорт в api/index.ts: добавлено расширение .js

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { createApp } from '../src/server/app'
+import { createApp } from '../src/server/app.js'
 
 // Vercel serverless function wrapper
 // This exports the Express app so Vercel can handle all /api/* routes
